7f84ee603523f1d186b12b0de03accc934d9c00d
Mellou46 <aalexfun79@gmail.com>
2021-08-08 22:02:51 +0300

    Unit 4 h/w, only "string reverse" method was realized.

--- src/main/java/ua/com/alevel/unit3_hw/PrimitivesHomeWorkMain.java
+++ src/main/java/ua/com/alevel/unit3_hw/PrimitivesHomeWorkMain.java
@@ -3,6 +3,7 @@ package ua.com.alevel.unit3_hw;
 public class PrimitivesHomeWorkMain {
     public static void main(String[] args) {
         System.out.println("hello world");
-
+        String s = new StringBuilder("Hello world").substring(0,7);
+        System.out.println("********** " + s);
     }
 }
